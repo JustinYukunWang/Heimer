f4e9a8e0cc69af55cdd112838ff208be0d16d026
Yukun Wang <wangyukun721@gmail.com>
2024-12-26 00:49:32 -0500

    Started player's data process

--- higher_rank_data_fetch.py
+++ higher_rank_data_fetch.py
@@ -7,6 +7,6 @@ cass.set_riot_api_key("RGAPI-c88daaab-100d-4fa6-8a4b-d0ee048d0737")
 
 masters = cass.get_master_league(queue=Queue.ranked_solo_fives, region='NA')
 player1 = masters[0].summoner.account
-data = fetch.fetch_player_data_with_account(player1, 1)
+data = fetch.player_data_with_account(player1, 1)
 
 fetch.save_to_spreadsheet(data, r"C:\Users\IKUN\Desktop\League Project\data", "higher_rank")
